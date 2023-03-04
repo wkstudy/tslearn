51517df27f89e74bcc343dcfbcce55cef0602488
wkstudy <1346948470@qq.com>
2023-03-04 17:54:49 +0800

    tmp

---
+++ practice.ts
@@ -0,0 +1,40 @@
+// Partial
+
+type IPartical<Type> = {
+  [x in keyof Type]?: Type[x]
+}
+// Required<Type>
+type IRequire<Type> = {
+  [x in keyof Type]: Type[x]
+}
+// Readonly<Type>
+type IReadonly<Type> = {
+  readonly[x in keyof Type]: Type[x]
+}
+// Record<Keys, Type>
+type IRecord<Key extends string | number | symbol, Type> = {
+  readonly[x in  Key]: Type
+}
+
+
+// Pick<Type, Keys>
+type IPick<Type, Keys extends keyof Type> = {
+  [k in Keys]: Type[k]
+}
+
+// Omit<Type, Keys>
+type IOmit<Type, Keys extends keyof Type> = {
+  [k in Exclude<keyof Type, Keys>]: Type[k]
+}
+
+// Exclude<UnionType, ExcludedMembers>
+//  ?
+type IExclude<UnionType, Keys> = ;
+type T0 = Exclude<"a" | "b" | "c", "a"| "b">;
+// type T0 = "b" | "c"
+
+
+// ? 
+type IExclude2<Type, Keys extends keyof Type>= {
+  [k in  keyof Type] : k
+}
